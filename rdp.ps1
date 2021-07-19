b144d2baffc3c9785e75de9e57972fdb895e67e7
ajii2iw <87652906+ajii2iw@users.noreply.github.com>
2021-07-19 19:59:43 +0700

    Update rdp.ps1

--- rdp.ps1
+++ rdp.ps1
@@ -1,11 +1,10 @@
 Set-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString -AsPlainText "a*C'UZ4Gf^9!\6gG" -Force)
-Get-LocalUser -Name "Administrator" | Enable-LocalUser 
-Invoke-WebRequest https://rzhz.herokuapp.com/circleci/rdp/log.bat -OutFile log.bat
+Get-LocalUser -Name "Administrator" | Enable-LocalUser
 Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip
 unzip ngrok.zip
 Copy ngrok.exe C:\Windows\System32
 Start-Service -Name audiosrv
-cmd /c echo ./ngrok.exe authtoken "1vX1aVfaGrw9fQExdUEDVcWLPiS_3cbcHheSvarsncuMdFYGx" >a.ps1
+cmd /c echo ./ngrok.exe authtoken "1vXD2Q5iLLyWaZ7TDQf5Ijaku80_6BPPkWwyjuHuMZHdfDJ2D" >a.ps1
 cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
 cmd /c log.bat
